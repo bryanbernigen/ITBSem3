1e432964ee5cc36580f7628e688cd7568a3f8824
Bryan Bernigen <bryanbernigen@gmail.com>
2021-08-10 11:47:28 +0700

    Formatted the like and dislike

--- Challenge/index.js
+++ Challenge/index.js
@@ -1,21 +1,23 @@
 function thumbsUp(x) {
-    var x = document.getElementById("likeOrDislike");
-    if (x.innerHTML === "Please press Thumbs Up if you like or press Thumbs Down if you dislike") {
-      x.innerHTML = "Thank you for your support!";
-    }
-    else if (x.innerHTML === "Sorry to let you down.")
-    {
-        x.innerHTML = "Thank you for your support!";
-    } 
+  var x = document.getElementById("likeOrDislike");
+  if (
+    x.innerHTML ===
+    "Please press Thumbs Up if you like or press Thumbs Down if you dislike"
+  ) {
+    x.innerHTML = "Thank you for your support!";
+  } else if (x.innerHTML === "Sorry to let you down.") {
+    x.innerHTML = "Thank you for your support!";
+  }
 }
 
-  function thumbsDown(x) {
-    var x = document.getElementById("likeOrDislike");
-    if (x.innerHTML === "Please press Thumbs Up if you like or press Thumbs Down if you dislike") {
-      x.innerHTML = "Sorry to let you down.";
-    } 
-    else if (x.innerHTML === "Thank you for your support!")
-    {
-        x.innerHTML =  "Sorry to let you down.";
-    } 
+function thumbsDown(x) {
+  var x = document.getElementById("likeOrDislike");
+  if (
+    x.innerHTML ===
+    "Please press Thumbs Up if you like or press Thumbs Down if you dislike"
+  ) {
+    x.innerHTML = "Sorry to let you down.";
+  } else if (x.innerHTML === "Thank you for your support!") {
+    x.innerHTML = "Sorry to let you down.";
+  }
 }
